c69e12c5a62e9ff2b869df9dbf188915b48bd082
wangjie <1540302851@qq.com>
2022-04-16 21:32:43 +0800

    docs

--- instance/lifecycle.js
+++ instance/lifecycle.js
@@ -1,5 +1,6 @@
 import { query } from '../utils'
 import { Watcher } from '../observer/watcher'
+import { patch } from '../vdom/patch'
 
 export function lifecycleMixin(Vue) {
     Vue.prototype._update = function(vnode) {
@@ -31,6 +32,7 @@ export function mountComponent(vm, el) {
         // watcher中使用call实现this指向vm实例
         this._update(this._render())
     }
-
+    // 执行update，挂载（更新）组件，数据的响应式触发时，此时的watcher是render watcher。
+    // 页面中的数据都收集了render watcher，当数据变化时，触发render watcher实现页面更新。
     new Watcher(vm, updateComponent)
 }
