c3b496421c0fa7ec3292a68aa642c61cb51f8160
wangjie <1540302851@qq.com>
2022-04-17 16:23:37 +0800

    feat createElement增加tag是组件的处理

--- vdom/create-element.js
+++ vdom/create-element.js
@@ -12,7 +12,7 @@ import { resoveAsset } from '../utils'
 export function createElement(context, tag, data, children) {
     let vnode
     if(typeof tag === 'string') {
-        let Ctor, BaseCtor = context.$options._base; // Vue（构造函数）
+        let Ctor
         // 如果是局部 / 全局组件
         if( Ctor = resoveAsset(context.$options, 'components', tag) ) {
             // Vue.extend 
@@ -22,5 +22,8 @@ export function createElement(context, tag, data, children) {
             vnode = new VNode(tag, data, children, undefined, undefined, context)
         }
         return vnode
+    } else {
+        // new Vue({ render: h => h(App)})此时tag是App组件
+        Ctor = createComponent(tag, data, children, context)
     }
 }
